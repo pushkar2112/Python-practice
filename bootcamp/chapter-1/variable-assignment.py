552d3d48ab0a4439032411dad5519830c091028b
Pushkar <Pushkar7397@gmail.com>
2021-05-10 21:31:35 +0530

    variable assignment

---
+++ bootcamp/chapter-1/variable-assignment.py
@@ -0,0 +1,19 @@
+# python is dynamically typed
+# this means that we dont need to define the type of the variable
+
+fname = 'Pushkar'
+lname = 'Verma'
+no = 99
+
+# variable naming rules apply
+# no special symbols
+# no numbers
+# no blank spaces
+
+print('My name is ',fname,lname)
+
+no = no + no
+print(no)
+
+fname = 101
+print(fname)
