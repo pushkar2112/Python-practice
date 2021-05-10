79c33c0a506763365082a2fe19c5f2305d1c2212
Pushkar <Pushkar7397@gmail.com>
2021-05-10 21:56:21 +0530

    string printing

---
+++ bootcamp/chapter-1/strings.py
@@ -0,0 +1,13 @@
+# Strings are used in Python to record text information, such as names.
+# Strings in Python are actually a sequence, which basically means Python keeps track 
+# of every element in the string as a sequence. 
+# For example, Python understands the string "hello' to be a sequence of letters in a specific order. 
+# This means we will be able to use indexing to grab particular letters 
+# (like the first letter, or the last letter).
+
+
+s1 = 'hello'
+s2 = "This is a sample string"
+
+print('This is another sample string')
+print('it"s an example to show how to use a quote in a quote')
