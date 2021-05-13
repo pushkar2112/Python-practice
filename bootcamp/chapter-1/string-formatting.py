848217966bd4d13c79e5a7aa5f31ec2feb6663af
Pushkar <Pushkar7397@gmail.com>
2021-05-13 20:23:12 +0530

    string\print formatting

---
+++ bootcamp/chapter-1/string-formatting.py
@@ -0,0 +1,19 @@
+# String formatting 
+
+# using .format()
+
+name = 'Sam'
+age = 17
+ctry = 'India'
+
+print('His name is {n}, his age is {a}, he is from {c}'.format(n='Sam',a=17,c='INDIA'))
+print('His name is {n}, his age is {a}, he is from {c}'.format(n=name,a=age,c=ctry))
+print('His name is {}, his age is {}, he is from {}'.format(name, age, ctry))
+
+# using f-strings
+
+print(f'His name is {name}, his age is {age}, he is from {ctry}')
+
+#           to get string representation
+print(f'His name is {name!r}, his age is {age}, he is from {ctry!r}')
+
