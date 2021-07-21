fdca521ac094a1fa8c0aa663d474790a1bdc0912
Pushkar <Pushkar7397@gmail.com>
2021-07-21 13:04:21 +0530

    while loops

--- bootcamp/chapter-3/while-loops.py
+++ bootcamp/chapter-3/while-loops.py
@@ -17,3 +17,17 @@ else:
 break: Breaks out of the current closest enclosing loop.
 continue: Goes to the top of the closest enclosing loop.
 pass: Does nothing at all.'''
+
+# Example
+x = 0
+
+while x < 10:
+    print('x is currently: ',x)
+    print(' x is still less than 10, adding 1 to x')
+    x+=1
+    if x==3:
+        print('Breaking because x==3')
+        break
+    else:
+        print('continuing...')
+        continue
