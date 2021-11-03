4c0285df685d29e25449f19300cc9687b4e444c6
Pushkar <Pushkar7397@gmail.com>
2021-11-03 17:55:14 +0530

    objects example

--- bootcamp/python-oop/circle-object.py
+++ bootcamp/python-oop/circle-object.py
@@ -4,12 +4,12 @@ class Circle:
     # Circle gets instantiated with a radius (default is 1)
     def __init__(self, radius=1):
         self.radius = radius 
-        self.area = radius * radius * Circle.pi
+        self.area = radius * radius * Circle.pi  # Circle.pi has been used as expained below
 
     # Method for resetting Radius
     def setRadius(self, new_radius):
         self.radius = new_radius
-        self.area = new_radius * new_radius * self.pi
+        self.area = new_radius * new_radius * self.pi  # Circle.pi can be used as well as pi is a class object attribute
 
     # Method for getting Circumference
     def getCircumference(self):
